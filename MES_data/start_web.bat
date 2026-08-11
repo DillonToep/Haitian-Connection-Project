37650068d9990171ef7fd1d1abecfb23e3c6cd37
DillonToep <dillon.toep@gmail.com>
2026-08-11 15:32:52 +0800

    Make main dashboard page home for all detailed metrics

--- MES_data/start_web.bat
+++ MES_data/start_web.bat
@@ -3,7 +3,7 @@ chcp 65001 >nul
 cd /d "%~dp0"
 echo 正在启动 MES 网页服务...
 echo 本机地址：http://127.0.0.1:8000
-echo 局域网地址：http://192.168.1.8:8000
+echo 局域网地址：http://192.168.1.9:8000
 echo.
 python -m uvicorn backend.main:app --app-dir "E:\Project main\sql-mqtt" --host 0.0.0.0 --port 8000
 echo.
