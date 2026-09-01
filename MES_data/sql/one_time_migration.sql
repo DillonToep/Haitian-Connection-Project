59acbcc108ed583936da36914b1fb21858b77921
DillonToep <dillon.toep@gmail.com>
2026-09-01 13:46:27 +0800

    Update

--- MES_data/sql/one_time_migration.sql
+++ MES_data/sql/one_time_migration.sql
@@ -1,42 +1,34 @@
 USE MES_MQTT;
 GO
 
--- mold_match_attempts: drop + recreate both FKs with ON DELETE SET NULL
+-- mold_match_attempts
 IF EXISTS (SELECT 1 FROM sys.foreign_keys WHERE name = N'FK_mold_match_attempts_mold')
-BEGIN
     ALTER TABLE dbo.mold_match_attempts DROP CONSTRAINT FK_mold_match_attempts_mold;
-END;
 GO
 IF EXISTS (SELECT 1 FROM sys.foreign_keys WHERE name = N'FK_mold_match_attempts_machine_type')
-BEGIN
     ALTER TABLE dbo.mold_match_attempts DROP CONSTRAINT FK_mold_match_attempts_machine_type;
-END;
 GO
 
 ALTER TABLE dbo.mold_match_attempts
     ADD CONSTRAINT FK_mold_match_attempts_mold
-        FOREIGN KEY (matched_mold_id) REFERENCES dbo.molds(id) ON DELETE SET NULL;
+        FOREIGN KEY (matched_mold_id) REFERENCES dbo.molds(id);  -- NO ACTION (default)
 GO
 ALTER TABLE dbo.mold_match_attempts
     ADD CONSTRAINT FK_mold_match_attempts_machine_type
         FOREIGN KEY (matched_machine_type_id) REFERENCES dbo.mold_machine_types(id) ON DELETE SET NULL;
 GO
 
--- mold_detection_alerts: same latent issue, fix preemptively
+-- mold_detection_alerts (same shape, same fix)
 IF EXISTS (SELECT 1 FROM sys.foreign_keys WHERE name = N'FK_mold_detection_alerts_mold')
-BEGIN
     ALTER TABLE dbo.mold_detection_alerts DROP CONSTRAINT FK_mold_detection_alerts_mold;
-END;
 GO
 IF EXISTS (SELECT 1 FROM sys.foreign_keys WHERE name = N'FK_mold_detection_alerts_machine_type')
-BEGIN
     ALTER TABLE dbo.mold_detection_alerts DROP CONSTRAINT FK_mold_detection_alerts_machine_type;
-END;
 GO
 
 ALTER TABLE dbo.mold_detection_alerts
     ADD CONSTRAINT FK_mold_detection_alerts_mold
-        FOREIGN KEY (matched_mold_id) REFERENCES dbo.molds(id) ON DELETE SET NULL;
+        FOREIGN KEY (matched_mold_id) REFERENCES dbo.molds(id);  -- NO ACTION (default)
 GO
 ALTER TABLE dbo.mold_detection_alerts
     ADD CONSTRAINT FK_mold_detection_alerts_machine_type
